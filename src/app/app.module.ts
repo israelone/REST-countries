8cde8f7bcc30acc046055f7902efdd2850ae5d6f
israelone <itzisraelone@gmail.com>
2022-12-04 08:24:11 -0600

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainComponent } from './main/main.component';
+import { CountryViewComponent } from './country-view/country-view.component';
+import { CountryCardComponent } from './country-card/country-card.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { CountryDataComponent } from './country-data/country-data.component';
+import { IndividualCountryViewComponent } from './individual-country-view/individual-country-view.component';
+import { MultipleCountryViewComponent } from './multiple-country-view/multiple-country-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainComponent,
+    CountryViewComponent,
+    CountryCardComponent,
+    CountryDataComponent,
+    IndividualCountryViewComponent,
+    MultipleCountryViewComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FontAwesomeModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
