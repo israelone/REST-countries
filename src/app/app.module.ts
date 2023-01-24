9d2b2697f0c66062bf6ba5b7133d87060bb40139
israelone <itzisraelone@gmail.com>
2023-01-24 16:12:48 -0600

    Removed unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MainComponent } from './main/main.component';
-import { CountryViewComponent } from './country-view/country-view.component';
 import { CountryCardComponent } from './country-card/country-card.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
-import { CountryDataComponent } from './country-data/country-data.component';
 import { IndividualCountryViewComponent } from './individual-country-view/individual-country-view.component';
 import { MultipleCountryViewComponent } from './multiple-country-view/multiple-country-view.component';
 
@@ -16,9 +14,7 @@ import { MultipleCountryViewComponent } from './multiple-country-view/multiple-c
     AppComponent,
     NavBarComponent,
     MainComponent,
-    CountryViewComponent,
     CountryCardComponent,
-    CountryDataComponent,
     IndividualCountryViewComponent,
     MultipleCountryViewComponent,
   ],
